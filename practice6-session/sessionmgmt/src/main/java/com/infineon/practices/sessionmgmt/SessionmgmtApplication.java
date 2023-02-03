32871deda3b0df5bdf6576f1dc989ac212441f7d
iomegak12 <jd.ramkumar@gmail.com>
2023-02-03 16:33:00 +0530

    last commit

---
+++ practice6-session/sessionmgmt/src/main/java/com/infineon/practices/sessionmgmt/SessionmgmtApplication.java
@@ -0,0 +1,13 @@
+package com.infineon.practices.sessionmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionmgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionmgmtApplication.class, args);
+	}
+
+}
---
+++ practice6-session/sessionmgmt/src/test/java/com/infineon/practices/sessionmgmt/SessionmgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infineon.practices.sessionmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionmgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
